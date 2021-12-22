f09feae4c0e1108f1aa0911d23761fea56a81d25
mrrootcode <69852724+mrrootcode@users.noreply.github.com>
2021-12-22 15:53:39 -0800

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/SlamDevs/slam-mirrorbot")
-    buttons.buildbutton("Channel", "https://t.me/SlamMirrorUpdates")
+    buttons.buildbutton("All Channel", "https://t.me/SystemErrorNetwork")
+    buttons.buildbutton("Channel", "https://t.me/SystemerrorBD")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
